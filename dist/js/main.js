a35b480d0c8b681755910076c8545ed55dd1bae2
Sanomi <snrutledge@gmail.com>
2015-09-02 01:36:16 -0700

    changing colors, next button, done page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,78 +1,90 @@
 (function() {
 'use strict';
 
-    var data, currentSlideId = -1;
+  var data, intervalId, remainingSeconds, currentSlideId = -1;
 
-    function randColor() {
-        return '#'+Math.floor(Math.random()*16777215).toString(16);
+  function randColor() {
+      return '#'+Math.floor(Math.random()*16777215).toString(16);
+  }
+  function invertColor(hexTripletColor) {
+    var color = hexTripletColor;
+    color = color.substring(1);           // remove #
+    color = parseInt(color, 16);          // convert to integer
+    color = 0xFFFFFF ^ color;             // invert three bytes
+    color = color.toString(16);           // convert to hex
+    color = ("000000" + color).slice(-6); // pad with leading zeros
+    color = "#" + color;                  // prepend #
+    return color;
+  }
+
+  function rgbToHex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
     }
-    function invertColor(hexTripletColor) {
-        var color = hexTripletColor;
-        color = color.substring(1);           // remove #
-        color = parseInt(color, 16);          // convert to integer
-        color = 0xFFFFFF ^ color;             // invert three bytes
-        color = color.toString(16);           // convert to hex
-        color = ("000000" + color).slice(-6); // pad with leading zeros
-        color = "#" + color;                  // prepend #
-        return color;
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+  }                
+
+  var showNextSlide = function() {
+
+  var eventHandler = function() {
+      event.stopPropagation();
+      clearInterval(intervalId);
+      showNextSlide();
+      button.removeEventListener("click", eventHandler);
     }
 
-    function rgbToHex(rgb) {
-        rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-        function hex(x) {
-            return ("0" + parseInt(x).toString(16)).slice(-2);
-        }
-        return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-    }                
+  var button = document.querySelectorAll('button')[0];
+  var buttonListener = button.addEventListener("click", 
+   eventHandler);
 
-    var showNextSlide = function(slideObject) {
 
-    var body = document.querySelectorAll('body')[0];
-    body.style.backgroundColor = randColor();
-    var rgb = getComputedStyle(body)['background-color'];
-    var hex = rgbToHex(rgb);
-    body.style.color = invertColor(hex);
+  var body = document.querySelectorAll('body')[0];
+  body.style.backgroundColor = randColor();
+  var rgb = getComputedStyle(body)['background-color'];
+  var hex = rgbToHex(rgb);
+  body.style.color = invertColor(hex);
 
-    var slideObject = data[++currentSlideId];
+  var slideObject = data[++currentSlideId];
 
-    var remainingSeconds = slideObject.seconds;
-    var remainingSecondsDiv = document.getElementById('remainingSeconds');
-    var instructionsDiv = document.getElementById('instructions');
+  var remainingSeconds = slideObject.seconds;
+  var remainingSecondsDiv = document.getElementById('remainingSeconds');
+  var instructionsDiv = document.getElementById('instructions');
 
-    remainingSecondsDiv.textContent = remainingSeconds;
-    instructionsDiv.textContent = slideObject.instructions;
+  remainingSecondsDiv.textContent = remainingSeconds;
+  instructionsDiv.textContent = slideObject.instructions;
 
-    remainingSecondsDiv.textContent = remainingSeconds;
+  remainingSecondsDiv.textContent = remainingSeconds;
 
-    var intervalId = setInterval(function() {
+  var intervalId = setInterval(function() {
     remainingSecondsDiv.textContent = --remainingSeconds;
-    var button = document.querySelectorAll('button')[0]
-        console.log(button)
-        button.addEventListener("click", function() {
-            clearInterval(intervalId);
-            showNextSlide();
-        });
     if (remainingSeconds === 0) {
-        clearInterval(intervalId);
-        showNextSlide();
+      clearInterval(intervalId);
+      showNextSlide();
     }
-    }, 1000);
+}, 1000);
+
+    if (slideObject === data[data.length-1]) {
+        instructionsDiv.textContent = 'Done.'
+        remainingSecondsDiv.textContent = ' ';
+    }
 };
 
-    var request = new XMLHttpRequest();
-    request.open('GET', '/data.json', true);
+  var request = new XMLHttpRequest();
+  request.open('GET', '/data.json', true);
 
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-                      // Success!
-        data = JSON.parse(request.responseText).data;
-        showNextSlide();
-        }
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+                  // Success!
+    data = JSON.parse(request.responseText).data;
 
-    };
-    request.onerror = function() {
-       // There was a connection error of some sort
-    };
-    request.send();
+    showNextSlide();
+      }
+
+  };
+  request.onerror = function() {
+     // There was a connection error of some sort
+  };
+  request.send();
 
 })();
